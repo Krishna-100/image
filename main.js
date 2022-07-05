9e9b678b1db1655b7974f0ef78eb7d49cda7d5d7
Krishna-100 <107849496+Krishna-100@users.noreply.github.com>
2022-07-05 17:49:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format : 'png',
+    png_quality: 90
+});
+
+camera = document.getElementById('camera');
+
+Webcam.attach('#camera')
+
+function take_photo()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innnerHTML = '<img id="captured_image" src="'+data_uri+'"/>'
+    });
+}
+
+console.log("ml5 version:" , ml5.version);
+
+classifer = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/5WpXROTGu/model.json' ,modelloaded);
